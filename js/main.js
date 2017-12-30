57a59ab44c4db748e02c857727fce65c30fd6e21
David Halewood <davidnhalewood@hotmail.com>
2017-12-30 12:29:12 +0000

    Adding players to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  console.log("ready...")
+  $('#findPlayers').on('click', function(e) {
+    let player1 = $('#player1').val();
+    let player2 = $('#player2').val();
+
+
+
+    $('#player1').val('')
+    $('#player2').val('')
+    e.preventDefault();
+  });
 });
